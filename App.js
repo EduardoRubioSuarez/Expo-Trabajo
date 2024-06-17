bcaa2fdeb87c8012fcb0cf12be843b24109d5ea4
EduardoRubio <rubioeduardo41@gmail.com>
2024-06-16 17:45:35 -0700

    Commit bien

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchSection}>
+        <Image
+          source={{
+            uri: 'https://pipedream.com/s.v0/app_mvNhzR/logo/orig',
+          }}
+          style={styles.image}
+        />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Ingrese el pokemon..."
+        />
+        <Button
+          title="Buscar Pokemon"
+          onPress={() => {
+            // Lógica de búsqueda de pokemon
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f4f4',
+  },
+  searchSection: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'gray',
+    padding: 10,
+    paddingHorizontal: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    elevation: 5,
+  },
+  searchInput: {
+    flex: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    resizeMode: 'contain',
+    marginRight: 10,
   },
 });
